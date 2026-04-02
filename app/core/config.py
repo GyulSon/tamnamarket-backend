6c53ca5502c6e6e734ad745d083bcbe153b9f6d4
backtrap <b354b.36750@gmail.com>
2026-04-03 02:15:29 +0900

    [신규] MySQL 추가

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Settings:
     PROJECT_NAME: str = "TamnaMarket Backend"
     API_V1_STR: str = "/api/v1"
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "mysql+pymysql://root:password@127.0.0.1:3306/tamnamarket")
 
 settings = Settings()
